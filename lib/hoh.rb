bc94c6077ffdb6a9906239e23f703b5054603089
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-22 02:07:02 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,18 +43,19 @@ def naming_system
   # HoH all the way to Species!
   animals = {
   
-    :label "Kingdom"{,
+    label: "Kingdom"{,
       sub_category:{
-      :label "Phylum"{,
+      label: "Phylum"{,
       sub_category:{
-        :label "Class"{,
+        label: "Class"{,
         sub_category:{
-          :label "Order"{,
+          label: "Order"{,
           sub_category:{
-            :label "Family"{,
+            label: "Family"{,
             sub_category:{
-              :label "Genus"{,
-                :label "Species"{,
+              label: "Genus"{,
+              sub_category:{
+                label: "Species"{,
                 sub_category:nil
                   
                 }
